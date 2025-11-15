0bf8bf48ed4b7e34cd89c01064748984414b7aea
André <m.andre0215@gmail.com>
2025-11-14 23:30:03 -0300

    Repositorios Paciente e Medico com casos de teste

--- src/main/java/br/com/marcosandre/sistemaclinica/sistemadeclinica/model/repositorios/RepositorioMedico.java
+++ src/main/java/br/com/marcosandre/sistemaclinica/sistemadeclinica/model/repositorios/RepositorioMedico.java
@@ -17,7 +17,33 @@ public class RepositorioMedico {
     private static List<Medico> medicos;
     
     static{        
-        medicos = new ArrayList<>();        
+        medicos = new ArrayList<>();      
+        
+        // -------- MÉDICOS DE TESTE --------
+
+        Medico m1 = new Medico();
+        m1.setCrm("1001");
+        m1.setNome("Dr. João Batista");
+        m1.setEspecialidade("Clínico Geral");
+        m1.setContato("(81) 98888-1001");
+        m1.setSenha("123"); // senha simples p/ testes
+        medicos.add(m1);
+
+        Medico m2 = new Medico();
+        m2.setCrm("1002");
+        m2.setNome("Dra. Marina Costa");
+        m2.setEspecialidade("Pediatria");
+        m2.setContato("(81) 98888-2002");
+        m2.setSenha("123");
+        medicos.add(m2);
+
+        Medico m3 = new Medico();
+        m3.setCrm("1003");
+        m3.setNome("Dr. Ricardo Nunes");
+        m3.setEspecialidade("Ortopedia");
+        m3.setContato("(81) 98888-3003");
+        m3.setSenha("123");
+        medicos.add(m3);
     }
     
     public static void inserir(Medico m){
--- src/main/java/br/com/marcosandre/sistemaclinica/sistemadeclinica/model/repositorios/RepositorioPaciente.java
+++ src/main/java/br/com/marcosandre/sistemaclinica/sistemadeclinica/model/repositorios/RepositorioPaciente.java
@@ -17,7 +17,49 @@ public class RepositorioPaciente {
     private static List<Paciente> pacientes;
     
     static{        
-        pacientes = new ArrayList<>();        
+        pacientes = new ArrayList<>();     
+        
+        // --- PACIENTES DE TESTE ---
+
+        Paciente p1 = new Paciente();
+        p1.setCpf("11111111111");
+        p1.setNome("João da Silva");
+        p1.setEndereco("Rua A, 123");
+        p1.setContato("(81) 90000-1111");
+        p1.setPlanoSaude("Unimed");
+        pacientes.add(p1);
+
+        Paciente p2 = new Paciente();
+        p2.setCpf("22222222222");
+        p2.setNome("Maria Oliveira");
+        p2.setEndereco("Av. Central, 456");
+        p2.setContato("(81) 90000-2222");
+        p2.setPlanoSaude("Hapvida");
+        pacientes.add(p2);
+
+        Paciente p3 = new Paciente();
+        p3.setCpf("33333333333");
+        p3.setNome("Carlos Pereira");
+        p3.setEndereco("Rua B, 890");
+        p3.setContato("(81) 90000-3333");
+        p3.setPlanoSaude("Amil");
+        pacientes.add(p3);
+
+        Paciente p4 = new Paciente();
+        p4.setCpf("44444444444");
+        p4.setNome("Ana Rodrigues");
+        p4.setEndereco("Travessa Verde, 45");
+        p4.setContato("(81) 90000-4444");
+        p4.setPlanoSaude("Bradesco Saúde");
+        pacientes.add(p4);
+
+        Paciente p5 = new Paciente();
+        p5.setCpf("55555555555");
+        p5.setNome("Pedro Santos");
+        p5.setEndereco("Av. Leste-Oeste, 999");
+        p5.setContato("(81) 90000-5555");
+        p5.setPlanoSaude("Sem plano");
+        pacientes.add(p5);
     }
     
     public static void inserir(Paciente p){
